169488bf8b111a5da73b0717ed47993131d67ea0
MinhNN <mr.noone1945@gmail.com>
2016-07-13 16:21:56 +0700

    Setup test env

--- WIP/Source/Veazy/src/test/java/vn/edu/fu/veazy/core/model/ExModelTest.java
+++ WIP/Source/Veazy/src/test/java/vn/edu/fu/veazy/core/model/ExModelTest.java
@@ -1,26 +1,40 @@
 package vn.edu.fu.veazy.core.model;
 
-import junit.framework.TestCase;
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 /**
 * Unit test for ExModel model.
 */
-public class ExModelTest extends TestCase {
 
-   public void testApp() {
-       SessionFactory sessionFactory = new Configuration().configure()
-               .buildSessionFactory();
-       Session session = sessionFactory.openSession();
-       session.beginTransaction();
+@WebAppConfiguration
+@ContextConfiguration("appContext.xml")
+public class ExModelTest {
+    
+    @Autowired
+    private SessionFactory sessionFactory;
+//    
+//    @Before
+//    public void setUp() {
+//        sessionFactory = new Configuration().configure()
+//                .buildSessionFactory();
+//    }
 
-       ExModel user = new ExModel("firstuser");
-       session.save(user);
+    @Test
+    public void testExModel() {
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
 
-       session.getTransaction().commit();
-       session.close();
-   }
+        ExModel user = new ExModel("firstuser");
+        session.save(user);
+
+        session.getTransaction().commit();
+        session.close();
+    }
 }
