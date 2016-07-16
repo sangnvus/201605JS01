9879a8ed6d38e7d1662ef6f7f4d971e6b88840e6
Đào Thanh Tùng <trumdongnat@gmail.com>
2016-07-16 08:47:48 +0700

    add annotation

--- WIP/Source/Veazy/src/main/java/vn/edu/fu/veazy/core/model/LessonModel.java
+++ WIP/Source/Veazy/src/main/java/vn/edu/fu/veazy/core/model/LessonModel.java
@@ -1,9 +1,22 @@
 package vn.edu.fu.veazy.core.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+@Entity
+@DynamicInsert
+@DynamicUpdate
+@Table(name = "`Lesson`")
 public class LessonModel extends BasicModel{
+	@Column(name = "index", columnDefinition="UNIQUE", nullable = false)
 	private int index;
+	@Column(name = "courseid", nullable = false)
 	private String courseId;
+	@Column(name = "versionId", nullable = false)
 	private String currentVersionId;
 	
 	public LessonModel() {
