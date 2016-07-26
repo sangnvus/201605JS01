b21c297ddbd98e0fa1018fd707e34ddf5ac60d9d
Đào Thanh Tùng <trumdongnat@gmail.com>
2016-07-26 16:44:53 +0700

    change URLMAPPING_GET_LESSON_COURSE

--- WIP/Source/Veazy/src/main/java/vn/edu/fu/veazy/core/common/Const.java
+++ WIP/Source/Veazy/src/main/java/vn/edu/fu/veazy/core/common/Const.java
@@ -32,7 +32,7 @@ public class Const {
     public static final String URLMAPPING_UPDATE_LESSON = URL_PREFIX + "/lessons/update/{lesson_id}";
     public static final String URLMAPPING_PUBLISH_LESSON = URL_PREFIX + "/lessons/publish/{lesson_id}";
     public static final String URLMAPPING_REPORT_LESSON = URL_PREFIX + "/lessons/report/{lesson_id}";
-    public static final String URLMAPPING_GET_LESSON_COURSE = URL_PREFIX + "/courses/{course_id}";
+    public static final String URLMAPPING_GET_LESSON_COURSE = URL_PREFIX + "/courses/getlessons/{course_id}/";
     public static final String URLMAPPING_GET_LESSON = URL_PREFIX + "/lessons/get/{lesson_id}";
     public static final String URLMAPPING_DELETE_LESSON = URL_PREFIX + "/lessons/delete/{lesson_id}";
     
